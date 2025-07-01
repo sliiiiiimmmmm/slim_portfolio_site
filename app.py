54a7624146d708a124b847ce9889625b5b4f7234
sliiiiiimmmmm <jorgeporragas@gmail.com>
2025-07-01 21:30:56 +0000

    Added context to some lines.

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask
 # Create app object of Flask class
 app = Flask(__name__)
 
+# This is the route for the default URL
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+# This will run the app on port 8080
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
